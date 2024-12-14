0674bce6bc21f4f2d370186d306754575ad60b55
whqr <whqr@Noutbuk-Nikita.local>
2024-12-14 11:44:04 +0300

    reworked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-import Image from "next/image";
+"use client"
+
+import axios from "axios";
+
+
 
 export default function Home() {
-  return (
+  const url = `
+  https://api.open-meteo.com/v1/forecast?
+  latitude=55.751244&
+  logitude=37.618423&
+  current=temperature_2m,is_day,wind_speed_10m
+  `;
   
+  fetch(url).then((data) => {
+    if(data.status == 200){
+      return data
+    }
+  })
+
+
+  return (
+  <div>
+    <h1>{data}</h1>
+  </div>
   );
 }
